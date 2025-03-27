be9be436f46dcc5e6bd54c5a95c30e53065f93fd
Caeleb Minor <minorcd@yahoo.com>
2025-03-27 08:51:30 -0400

    Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Lessons from './pages/Lessons';
 
 const App = () => {
     return (
-       <div>
-            <h1>A-Z Who God Says I Am</h1>
-       </div>
-    );
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} /> {/* Home Page */}
+                <Route path="/about" element={<About />} /> {/* About Page */}
+                <Route path="/lessons" element={<Lessons />} /> {/* List of Lessons */}
+            </Routes>
+        </Router>
+    )
 };
 
 export default App;
